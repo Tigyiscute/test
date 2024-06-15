385519435d6b93e749b31aa8da3b8498920a1592
Panther's Cheats <142128094+Tigyiscute@users.noreply.github.com>
2024-06-14 17:19:34 -0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.querySelector('.restore-key').addEventListener('click', function() {
-    alert('Key restored!');
-});
-
-document.querySelector('.run-script').addEventListener('click', function() {
-    alert('Script running!');
-});
