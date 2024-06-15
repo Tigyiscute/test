38767f5597f2fb5a71b308b2877c565380ba1730
Panther's Cheats <142128094+Tigyiscute@users.noreply.github.com>
2024-06-14 17:17:10 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelector('.restore-key').addEventListener('click', function() {
+    alert('Key restored!');
+});
+
+document.querySelector('.run-script').addEventListener('click', function() {
+    alert('Script running!');
+});
